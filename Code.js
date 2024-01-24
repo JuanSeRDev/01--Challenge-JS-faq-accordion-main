ca4d1d65f757c5b36dbbce5d0344bcafab9e725d
Sebastian Rodriguez <144717496+SebastianRodriguezCode@users.noreply.github.com>
2024-01-24 12:52:04 -0500

    challenge Finished

--- Code.js
+++ Code.js
@@ -2,15 +2,17 @@
 
 const acordeones = document.querySelectorAll(".acordeon");
 const titulo = document.querySelectorAll(".h3");
+const image = document.querySelectorAll(".img");
 
 titulo.forEach((cadaTitulo, i)=>{
     titulo[i].addEventListener("click", ()=>{
-        if (acordeones[i].classList.contains("active")){
+        if (acordeones[i].classList.contains("active") & image[i].classList.contains("minus")){
             acordeones[i].classList.remove("active");
+            image[i].classList.remove("minus");
         } else{
-            acordeones[i].classList.add("active")
+            acordeones[i].classList.add("active");
+            image[i].classList.add("minus");
         }
-        
     })
 });
 
